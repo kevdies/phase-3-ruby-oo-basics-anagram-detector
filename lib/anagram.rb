546d1006fd4a7afcca9311dec720cfae75764bf8
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-13 13:06:51 -0500

    commit tree 2719a2f410cacd7a4a3677750f1b375df2e145f8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+    def initialize(word)
+        @name = word
+    end
+# match method filters???
+# if the name split it to array and sort it is equal to the arr of words split and sorted
+# how are we returning an empty array if not denoted in the block???
+    def match(array_of_words)
+         array_of_words.filter {|word| word.split("").sort == @name.split("").sort }
+
+    end
+end
